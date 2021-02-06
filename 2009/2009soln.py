2a971997607406e247fa84c568bf9d2dfa75f30c
Winston Cheong <cheong.winston1337@gmail.com>
2021-02-06 00:57:41 -0600

    Finished 2009

---
+++ 2009/2009soln.py
@@ -0,0 +1,108 @@
+import sys 
+
+if len(sys.argv) > 1:
+    file = sys.argv[1]
+else: 
+    file = 'sample1.txt'
+f = open(file)
+
+class Window:
+
+    def __init__(self, x, y, width, height, zorder):
+        self.left = x
+        self.top = y
+        self.right = x + width - 1
+        self.bottom = y + height - 1
+        self.zorder = zorder
+
+    def __repr__(self):
+        pass
+
+    def enforce_screen(self, screenX, screenY):
+        """Trim parts of window that are outside of screen"""
+
+        if self.left < 0:
+            self.left = 0
+        if self.top < 0: 
+            self.top = 0
+        if self.right >= screenX:
+            self.right = screenX - 1
+        if self.bottom >= screenY:
+            self.bottom = screenY - 1
+
+
+        
+
+def calculate_visible(windows, i):
+    """Calculate visible portion of the i-th window. 
+    Output coordinates of visibility or "completely obscured"
+    """
+    # Must not modify the actual window's dimensions! 
+    # The window may be used to obscure other windows!
+
+    window = windows[i]
+    visible_left = window.left
+    visible_right = window.right
+    visible_top = window.top
+    visible_bottom = window.bottom
+
+
+    can_cover =  [w for w in windows if w.zorder > window.zorder]
+
+    for w in can_cover:
+        # check if obscures left
+
+        if (w.top <= visible_top and w.bottom >= visible_bottom):
+            # possible to obscure left side or right side
+
+            if w.left <= visible_left and w.right >= visible_left:
+                # obscures left side
+                visible_left = w.right + 1
+
+            if w.left <= visible_right and w.right >= visible_right:
+                # obscures right side
+                visible_right = w.left - 1
+
+        if (w.left <= visible_left and w.right >= visible_right):
+            # possible to obscure top side or bottom side
+
+            if w.top <= visible_top and w.bottom >= visible_top:
+                # obscures top
+                visible_top = w.bottom + 1
+
+            if w.top <= visible_bottom and w.bottom >= visible_bottom:
+                # obscures bottom
+                visible_bottom = w.top - 1
+
+    #print(f'({visible_left}, {visible_top}) to ({visible_right}, {visible_bottom})')
+
+    if visible_left > visible_right or visible_top > visible_bottom:
+        return "completely obscured"
+    return f'({visible_left}, {visible_top}) to ({visible_right}, {visible_bottom})'
+
+num_datasets = int(f.readline())
+print(f'Analyzing {num_datasets} Data sets')
+
+for i in range(num_datasets):
+    print(f' Data set {i+1}:')
+
+    screenX, screenY = map(int, f.readline().split())
+    print(f'  Screen size: {screenX} x {screenY}')
+
+    num_windows = int(f.readline())
+    print(f'  Windows: {num_windows}')
+
+    windows = []
+    # read in windows
+    for j in range(num_windows):
+        w = Window(*map(int, f.readline().split()))
+        w.enforce_screen(screenX, screenY)
+        windows.append(w)
+
+    # output visible area of each window
+    for j in range(num_windows):
+        print(f'   Window {j+1}: {calculate_visible(windows, j)}')
+    
+
+
+    # reset for new dataset
--- README.md
+++ README.md
@@ -1,3 +1,33 @@
 The programming contest files are hosted at http://elvis.rowan.edu/rupc/
 
 The directories marked `-old` are from the dark ages when Java was my main language (high school to early undergrad)
+
+Python was only added as an allowed language in 2012. Before then, the allowed langages were Java, 
+C++ and Basic. I will ignore this.
+
+
+Note: I have no real way of knowing whether my code is correct.. the test inputs seem to be tricker, with corner cases that the sample inputs don't illustrate. 
+
+----
+
+# 2009
+Windows and Z-order
+* Simplifying assumption: any window will either completely cover another window, or leave exposed a rectangular portion of any window it covers (no covering only a corner).
+* Higher z-order means it is on top. Z-order will be positive integer.
+* Visible area of any windows will be contiguous
+
+## Input spec
+* integer: number of datasets
+* for each dataset:
+  * width, heigth
+  * #windows
+  * 5 integers: 
+    * (x, y) coord of upper-left corner
+    * width, height of window
+    * z order
+
+## Output
+* some input validation / printout:
+  * #data sets, which dataset, screensize, #windows
+* For each window (printed in order read in):
+  * Visible portion (upper-left coord to lower-right coord) OR "completely obscured"
